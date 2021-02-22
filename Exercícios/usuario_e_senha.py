3928b4d7766b5b0d680ed7228970e7d02f58bc35
Your Name <you@example.com>
2021-02-22 20:58:22 -0300

    elif

---
+++ Exercícios/usuario_e_senha.py
@@ -0,0 +1,20 @@
+usuario = input("Informe o usuário do sistema!")
+
+#if(usuario == "Flávio"):
+#    print("Seja bem-vindo Flávio!")
+#else(usuario == "Douglas"):
+#    print("Seja bem-vindo Douglas!")
+#else(usuario == "Nico"):
+#    print("Seja bem-vindo Nico")
+#else:
+#    print("Usuário não identificado!")
+#
+
+if usuario == "Flávio":
+    print("Seja bem-vindo Flávio!")
+elif usuario == "Douglas":
+    print("Seja bem-vindo Douglas!")
+elif usuario == "Nico":
+    print("Seja bem-vindo Nico")
+else:
+    print("Usuário não identificado!")
