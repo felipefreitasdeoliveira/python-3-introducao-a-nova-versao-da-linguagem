a82d1b75fcf8a62ac680f0030d0f00ab07badf68
Felipe Freitas <oliveife@gmail.com>
2021-02-22 22:07:18 -0300

    Add for and while in this file

---
+++ Exercícios/while.py
@@ -0,0 +1,10 @@
+contador = 1
+while contador <= 10:
+    print(contador)
+    contador = contador + 1
+
+
+# Ou poderia ser
+
+for contador in range(1, contador + 11):
+    print(contador)
--- adivinhacao.py
+++ adivinhacao.py
@@ -4,12 +4,16 @@ print('*********************************')
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
-while rodada <= total_de_tentativas:
-    print("Tentativa", rodada, "de", total_de_tentativas)
-    chute_str = int(input("Digite o seu número: "))
+
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = int(input("Digite um número entre 1 e 100 : "))
     print("Você digitou ", chute_str)
     chute = int(chute_str)
+
+    if chute_str < 1 or chute > 100:
+        print("Digite um número entre 1 e 100")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -17,10 +21,10 @@ while rodada <= total_de_tentativas:
 
     if acertou:
         print("Você acertou!")
+        break
     else:
         if maior:
             print("Você errou! O seu chute foi maior do que o número secreto.")
         elif menor:
             print("Você errou! O seu chute foi menor do que o número secreto.")
-    rodada = rodada + 1
-print("Fim do Jogo")
+print("\n Fim do Jogo")
