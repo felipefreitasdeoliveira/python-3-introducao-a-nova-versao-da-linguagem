79c5d5d65aca57b640e899c6eedfdac53c8c93ed
Felipe Freitas <felipe@compwave.com.br>
2021-02-23 18:51:31 -0300

    ajustes nos arquivos

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,11 +6,8 @@ print('Bem vindo ao jogo de Adivinhação!')
 print('*********************************')
 
 
-
-numero_secreto = random.randrange (1,101)
+numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 3
-
-print(numero_secreto)
 
 
 for rodada in range(1, total_de_tentativas + 1):
