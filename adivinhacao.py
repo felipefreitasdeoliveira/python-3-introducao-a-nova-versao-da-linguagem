137347ddb7bdc602ef937543dd3103cb58378d9e
Felipe Freitas <felipe@compwave.com.br>
2021-03-21 19:06:15 -0300

    add pontuacao

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,25 +1,26 @@
 #! python3
 
 import random
+
 print('*********************************')
 print('Bem vindo ao jogo de Adivinhação!')
 print('*********************************\n')
 
-
 numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 0
+pontos = 1000
 
-print("Qual nível de Dificuldade?")
+
+print("Qual nível de Dificuldade?", numero_secreto)
 print('*********************************')
 print("(1) Fácil - (2) Médio - (3) Difícil")
 print('*********************************\n')
 
 nivel = int(input("Defina o Nível: "))
-
 if nivel == 1:
     total_de_tentativas = 20
 elif nivel == 2:
-        total_de_tentativas = 10
+    total_de_tentativas = 10
 else:
     total_de_tentativas = 5
 
@@ -38,11 +39,15 @@ for rodada in range(1, total_de_tentativas + 1):
     menor = chute < numero_secreto
 
     if acertou:
-        print("Você acertou!")
+        print("Você acertou e fez {} pontos!".format(pontos))
         break
     else:
         if maior:
             print("Você errou! O seu chute foi maior do que o número secreto.")
         elif menor:
             print("Você errou! O seu chute foi menor do que o número secreto.")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
+
 print("\n Fim do Jogo")
+
