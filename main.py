ca293ae3e22fe8b9a00a51cfc827faaf1ee51d8c
Felipe Freitas <felipe@compwave.com.br>
2021-03-25 23:26:24 -0300

    Ajustes files

---
+++ main.py
@@ -0,0 +1,10 @@
+import Exercícios.idade_atual
+
+idade = Exercícios.idade_atual.calcula_idade(2021, 1986)
+
+print(idade)
+
+
+multiplica = Exercícios.idade_atual.calcula_multiplicacao(5, 5)
+
+print(multiplica)
