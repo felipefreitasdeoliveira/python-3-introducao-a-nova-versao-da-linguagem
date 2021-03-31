e3dac72833384598d68530aced9886e7bc88a333
Felipe Freitas <oliveife@gmail.com>
2021-03-31 10:43:50 -0300

    Ajuste Main

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import Exercícios.Parte01.idade_atual
 
-idade = Exercícios.Parte01.idade_atual.calcula_idade(2021, 1986)
+idade = Exercícios.Parte01.idade_atual.calcula_idade(2021, 1985)
 
 print(idade)
-
-
-multiplica = Exercícios.Parte01.idade_atual.calcula_multiplicacao(5, 5)
-
-print(multiplica)
